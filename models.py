2e2854cd2aca84e518d1ece6c3e8f2fbc6adb537
abigailelizabeth <abbyeb22@gmail.com>
2016-03-16 03:22:47 -0500

    created user class

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
 from init import db, app
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    firstName = db.Column(db.String(15))
+    lastName = db.Column(db.String(15))
+
 class LostPosting(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50))
